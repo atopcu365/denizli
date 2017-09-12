dbde09170861f82fb4d5db8a4a051baddb288367
Ali Topcu <atopcu365@gmail.com>
2017-09-11 21:43:23 -0400

    Initial Commit

---
+++ Denizli/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Denizli
+//
+//  Created by Ali Topcu on 9/11/17.
+//  Copyright © 2017 Ali Topcu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
